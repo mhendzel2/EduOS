0e8952888817745d075cbb2b695622cf22f8f620
mhendzel2 <mhendzel@ualberta.ca>
2026-03-28 06:15:57 -0600

    feat: add requirements.txt to define backend dependencies

--- backend/baseos/services/__init__.py
+++
@@ -1,6 +0,0 @@
-"""BaseOS service imports for EduOS."""
-
-from .memory_compactor import MemoryCompactor, global_memory_compactor
-from .turboquant_compressor import TurboQuantCompressor
-
-__all__ = ["MemoryCompactor", "TurboQuantCompressor", "global_memory_compactor"]
--- backend/baseos/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-"""BaseOS tool imports for EduOS."""
-
-from .mcp_connector import MCP_AVAILABLE, McpConnector
-
-__all__ = ["MCP_AVAILABLE", "McpConnector"]
